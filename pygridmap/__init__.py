9902a7acb2e1bb3507a9d2b2de78d83bd5af1722
Jacopo Grazzini <gjacopo@protonmail.com>
2020-09-04 09:47:00 +0200

    Add files via upload

---
+++ pygridmap/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+.. __init__
+
+Initialisation module of `pygridmap` package.
+
+**Contents**
+"""
+
+# *credits*:      `gjacopo <jacopo.grazzini@ec.europa.eu>`_ 
+# *since*:        Jun 2020
+
+__THISFILE          = __file__ # useles...
+__THISDIR           = osp.dirname(__THISFILE)
+
+__all__             = ['base', 'gridding', 'overlay']#analysis:ignore
