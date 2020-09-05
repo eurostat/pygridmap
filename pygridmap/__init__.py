b317b7bd6f9d37edab8a83f06b9c02bde39dee70
Jacopo Grazzini <gjacopo@gmail.com>
2020-09-05 16:35:19 +0200

    Update __init__.py

--- pygridmap/__init__.py
+++ pygridmap/__init__.py
@@ -16,10 +16,3 @@ __THISFILE          = __file__ # useles...
 __THISDIR           = osp.dirname(__THISFILE)
 
 __all__             = ['base', 'gridding', 'overlay']#analysis:ignore
-
-#PROJS               = ["EPSG:4326", "EPSG:3035"]
-#"""Available projections.
-#""" See pyproj
-DEFPROJ             = "EPSG:4326" # "EPSG:3035"
-"""Default projection in calculations.
-"""
