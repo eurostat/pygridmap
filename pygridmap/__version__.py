fba69c58986a735ad4999397ca9e5a64a8fbba63
Jacopo Grazzini <gjacopo@protonmail.com>
2020-09-06 23:07:08 +0200

    Add files via upload

---
+++ pygridmap/__version__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+.. __version__
+
+Versioning module of package `pygridmap`.
+
+**Contents**
+"""
+
+# *credits*:      `gjacopo <jacopo.grazzini@ec.europa.eu>`_ 
+# *since*:        June 2020
+
+
+VERSION = (0, 0, 1)
+"""Current version.
+"""
+
+__version__ = '.'.join(map(str, VERSION))
