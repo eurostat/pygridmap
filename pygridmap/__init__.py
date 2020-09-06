23c1af1c286104e65ebd6596f406e0578a9513bc
Jacopo Grazzini <gjacopo@gmail.com>
2020-09-06 23:24:41 +0200

    Update __init__.py

--- pygridmap/__init__.py
+++ pygridmap/__init__.py
@@ -12,9 +12,30 @@ Initialisation module of `pygridmap` package.
 # *credits*:      `gjacopo <jacopo.grazzini@ec.europa.eu>`_ 
 # *since*:        June 2020
 
+# 88""Yb Yb  dP  dP""b8 88""Yb 88 8888b.  8b    d8    db    88""Yb 
+# 88__dP  YbdP  dP   `" 88__dP 88  8I  Yb 88b  d88   dPYb   88__dP 
+# 88"""    8P   Yb  "88 88"Yb  88  8I  dY 88YbdP88  dP__Yb  88"""  
+# 88      dP     YboodP 88  Yb 88 8888Y"  88 YY 88 dP""""Yb 88     
+                                                                
+#%% Settings
+
 from os import path as osp
 
-__THISFILE          = __file__ # useles...
-__THISDIR           = osp.dirname(__THISFILE)
+#%% Global vars
 
-__all__             = ['base', 'gridding', 'overlay']#analysis:ignore
+#__THISFILE          = __file__ # useles...
+#__THISDIR           = osp.dirname(__THISFILE)
+
+PACKNAME            = 'pygridmap' # this package...
+"""Name of this package.
+"""
+
+#PACKPATH            = getattr(sys.modules[PACKNAME], '__path__')[0]
+PACKPATH            = osp.dirname(__file__) 
+"""Path to this package.
+"""
+
+__modules           = ['base', 'gridding', 'overlay']#analysis:ignore
+
+__all__             = ['%s%s' % (__,BASENAME) for __ in __modules]
+__all__.extend(['__version__']) # , '__start__'
